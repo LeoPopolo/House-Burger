f8a2556861765c9b6cd0eb8509012015725575a1
LeoPopolo <leopopologuitarreta@gmail.com>
2020-07-24 02:55:19 +0200

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -3,10 +3,6 @@ $(function(){
     $("#sobreNosotros").click(function(){
         mostrarNosotros();
     });
-
-    $("#menu").click(function(){
-        mostrarMenu();
-    });
 });
 
 function cerrarContenedor(){
@@ -24,19 +20,6 @@ function mostrarNosotros(){
                     Laudantium maiores dignissimos nam, molestias, et inventore eligendi culpa, tempore sint molestiae quas veniam omnis. Sint non sit possimus omnis ipsa ipsum optio consequuntur, veritatis atque modi fugiat, quo numquam.
                     Quae quam vel consequatur molestiae odit dolorem quas voluptates veniam quis consectetur, earum assumenda cumque placeat id necessitatibus officia animi ut omnis at libero minus tempore recusandae? Ipsa, quia odio.
                     Soluta, provident enim! assumenda in quos delectus nam ab porro! Adipisci exercitationem optio eum ipsam blanditiis, assumenda quasi animi eos, amet dolorum earum aut rem obcaecati tenetur, est soluta! Laudantium!</p>
-                    </div>
-                `;
-
-    $("#contenedorPrincipal").append(cadena);
-    $("#contenedorPrincipal").slideToggle("slow");
-}
-
-function mostrarMenu(){
-    $("#contenedorPrincipal").empty();
-    let cadena = `
-                    <div id="divBtnCerrarContenedor"><button id="btnCerrarContenedor" onclick="cerrarContenedor();">X</button></div>
-                    <div id="fotoMenu">
-                        <img src="./img/menu.jpeg" width="100%">
                     </div>
                 `;
 
